c16ad5f8a5fc6b7a0604e7c10e2171de278b0df0
Václav Juřena <vaclav.jurena@inventi.cz>
2022-06-09 22:45:53 +0200

    Assignment Inheritance and Polymorphism

---
+++ 4-design-patterns/src/CarFactory/WarningMessages.java
@@ -0,0 +1,10 @@
+package CarFactory;
+
+public class WarningMessages {
+
+    public static final String EMPTYLINE = "Factory is not working...... All people have day off";
+    public static final String EMPTYWAREHOUSE = "Warehouse is empty, \n Maybe door is closed or workers are not working today";
+
+
+}
+
